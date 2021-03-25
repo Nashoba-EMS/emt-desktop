2fcced591c420003c62a03e4e7dd7f510bf710b6
Jeff Taylor-Chang <jeff@jefftc.com>
2021-03-25 17:26:17 -0400

    Added calendar to schedule page

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import moment from "moment";
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, Event, momentLocalizer } from "react-big-calendar";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
@@ -18,11 +18,13 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { ReduxState } from "../redux";
 import { _schedules } from "../redux/actions";
 import { Tab, Tabs } from "@material-ui/core";
+import { Schedule, ScheduleDay } from "../api/schedules.d";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      width: "100%"
+      width: "100%",
+      paddingBottom: theme.spacing(2)
     },
     paperTabs: {
       marginBottom: theme.spacing(2)
@@ -53,6 +55,18 @@ const useStyles = makeStyles((theme) =>
 
 const localizer = momentLocalizer(moment);
 
+const compareScheduleDay = (a: ScheduleDay, b: ScheduleDay) => {
+  if (a.date < b.date) {
+    return -1;
+  }
+
+  if (a.date > b.date) {
+    return 1;
+  }
+
+  return 0;
+};
+
 const SchedulePage: React.FC = () => {
   const classes = useStyles();
 
@@ -64,9 +78,11 @@ const SchedulePage: React.FC = () => {
   const schedules = useSelector((state: ReduxState) => state.schedules.schedules);
   const availability = useSelector((state: ReduxState) => state.schedules.availability);
   const isGettingAvailability = useSelector((state: ReduxState) => state.schedules.isGettingAvailability);
+  const isUpdatingSchedule = useSelector((state: ReduxState) => state.schedules.isUpdatingSchedule);
 
   const [visibleDate, setVisibleDate] = React.useState<Date>(new Date());
   const [tabIndex, setTabIndex] = React.useState<number>(0);
+  const [assignments, setAssignments] = React.useState<Schedule["assignments"]>([]);
 
   const schedule = React.useMemo(() => schedules.find((schedule) => schedule._id === schedule_id), [
     schedule_id,
@@ -91,14 +107,10 @@ const SchedulePage: React.FC = () => {
     [scheduleEnd, scheduleStart]
   );
 
-  const events = React.useMemo(() => {
-    let events: {
-      title: string;
+  const availabilityEvents = React.useMemo(() => {
+    let events: (Event & {
       user_id: string;
-      start: Date;
-      end: Date;
-      allDay: true;
-    }[] = [];
+    })[] = [];
 
     for (const availability of scheduleAvailability) {
       const cadetName = cadets.find((cadet) => cadet._id === availability.user_id)?.name ?? "Unknown";
@@ -123,10 +135,48 @@ const SchedulePage: React.FC = () => {
     return events;
   }, [cadets, isDayValid, scheduleAvailability]);
 
+  const scheduleEvents = React.useMemo(() => {
+    let events: (Event & {
+      user_id: string;
+    })[] = [];
+
+    for (const assignment of assignments) {
+      const date = moment(assignment.date).toDate();
+
+      events = events.concat(
+        assignment.cadet_ids.map((cadet_id) => ({
+          title: cadets.find((cadet) => cadet._id === cadet_id)?.name ?? "Unknown",
+          user_id: cadet_id,
+          start: date,
+          end: date,
+          allDay: true
+        }))
+      );
+    }
+
+    return events;
+  }, [assignments, cadets]);
+
+  const noModifications = React.useMemo(() => {
+    return (
+      JSON.stringify(assignments.sort(compareScheduleDay)) ===
+      JSON.stringify(schedule?.assignments.sort(compareScheduleDay))
+    );
+  }, [assignments, schedule?.assignments]);
+
   const dispatch = useDispatch();
   const dispatchGetAvailabilityForSchedule = React.useCallback(
     () => user?.admin && dispatch(_schedules.getAvailability(token, { schedule_id })),
     [dispatch, schedule_id, token, user?.admin]
+  );
+  const dispatchUpdateSchedule = React.useCallback(
+    () =>
+      dispatch(
+        _schedules.updateSchedule(token, schedule_id, {
+          assignments
+        })
+      ),
+    [assignments, dispatch, schedule_id, token]
   );
 
   const dayPropGetter = React.useCallback(
@@ -157,13 +207,17 @@ const SchedulePage: React.FC = () => {
   );
 
   /**
-   * Refresh the availability of the given user
+   * Refresh the availability of the given schedule
    */
   React.useEffect(() => {
     if (token) {
       dispatchGetAvailabilityForSchedule();
     }
   }, [dispatchGetAvailabilityForSchedule, token]);
+
+  React.useEffect(() => {
+    setAssignments(schedule?.assignments ?? []);
+  }, [schedule?.assignments]);
 
   React.useEffect(() => {
     setVisibleDate(scheduleStartDate);
@@ -223,11 +277,10 @@ const SchedulePage: React.FC = () => {
                 localizer={localizer}
                 defaultView="month"
                 views={["month"]}
-                selectable
                 showAllEvents
                 date={visibleDate}
                 onNavigate={(newDate) => setVisibleDate(newDate)}
-                events={events}
+                events={availabilityEvents}
                 dayPropGetter={dayPropGetter}
                 eventPropGetter={eventPropGetter}
                 style={{ width: "100%", height: 800 }}
@@ -258,6 +311,47 @@ const SchedulePage: React.FC = () => {
             satisfied with the schedule you can generate a new one. As a warning, generating a schedule can take some
             time, please do not navigate away while a schedule is being generated.
           </Typography>
+
+          <Paper className={classes.paper}>
+            <div className={classes.calendarContainer}>
+              <Calendar
+                localizer={localizer}
+                defaultView="month"
+                views={["month"]}
+                showAllEvents
+                date={visibleDate}
+                onNavigate={(newDate) => setVisibleDate(newDate)}
+                events={scheduleEvents}
+                dayPropGetter={dayPropGetter}
+                eventPropGetter={eventPropGetter}
+                style={{ width: "100%", height: 800 }}
+              />
+            </div>
+          </Paper>
+
+          <Button
+            className={classes.clearButton}
+            color="secondary"
+            disabled={noModifications}
+            onClick={() => setAssignments(schedule?.assignments ?? [])}
+          >
+            Clear Changes
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            startIcon={
+              isUpdatingSchedule ? (
+                <CircularProgress className={classes.spinner} color="inherit" size={16} />
+              ) : (
+                <SaveIcon />
+              )
+            }
+            disabled={noModifications || isGettingAvailability || isUpdatingSchedule}
+            onClick={dispatchUpdateSchedule}
+          >
+            Save Changes
+          </Button>
         </React.Fragment>
       )}
     </div>
